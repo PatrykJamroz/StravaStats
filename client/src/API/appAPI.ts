a91ab47e79a5cbb9cb84d059be1ea026d3226e7f
patrykjamroz <jamroz.patryk@gmail.com>
2022-12-19 13:31:44 +0100

    fix urls

--- client/src/API/appAPI.ts
+++ client/src/API/appAPI.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { Activity, Athlete } from '../models/Strava';
 
+const BASE_URL = 'https://statz-api.onrender.com';
+
 export const getAthlete = async (): Promise<Athlete> => {
-  return axios.get('/api/athlete').then(({ data }) => data);
+  return axios.get(`${BASE_URL}/api/athlete`).then(({ data }) => data);
 };
 
 export const getActivities = async (): Promise<Activity[]> => {
-  return axios.get('/api/activities').then(({ data }) => data);
+  return axios.get(`${BASE_URL}/api/activities`).then(({ data }) => data);
 };
